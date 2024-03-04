2fe6d4547c0b35adf41326cf054d0e1f1ed90b5f
mack lee <114943402+macklee251@users.noreply.github.com>
2024-03-04 14:34:17 -0300

    .

---
+++ python_install.sh
@@ -0,0 +1,25 @@
+# Para rodar esse arquivo digitamos chmod +x python_install.sh e depois ./python_install.sh
+echo "**/__pycache__" > .gitignore;   # Criando um arquivo .gitignore
+cat <<EOT >> .dockerignore
+**/__pycache__
+venv
+EOT
+python3 -m venv venv                  # Criando um ambiente virtual
+. venv/bin/activate                   # Ativando o ambiente virtual
+pip install -r requirements.txt       # Instalando as dependências do projeto  
+pip install pre-commit                # Instalando o pre-commit
+pre-commit install                    # Instalando o pre-commit
+cat <<EOT >> .pre-commit-config.yaml
+repos:
+  - repo: local
+    hooks:
+      - id: requirements
+        name: requirements
+        entry: bash -c 'venv/bin/pip3 freeze > requirements.txt; git add requirements.txt'
+        language: system
+        pass_filenames: false
+        stages: [commit]
+EOT
+git init
+git add .
+git commit -m "Config: initial configurations"
